fb05646f73d55290582059219ec32074096bf9de
Brian Bartholomew <me@bcbrian.com>
2019-08-20 12:01:33 -0500

    We can battle no we create the ui of the battle

--- index.js
+++ index.js
@@ -3,47 +3,109 @@ const API_KEY = "bUcx7xDKXvZQDgY629OF9v1fNqDFNImK";
 // api.giphy.com/v1/gifs/random
 
 function fetchRandomGif(category, cb) {
-  fetch(
+  return fetch(
     "https://api.giphy.com/v1/gifs/random?q=" + category + "&api_key=" + API_KEY
-  )
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(responseJson) {
-      cb(responseJson.data);
-    });
+  ).then(function(response) {
+    return response.json();
+  });
+  // .then(function(responseJson) {
+  //   cb(responseJson.data);
+  // });
 }
 
-fetchRandomGif("obama", function(giphy1) {
-  fetchRandomGif("trump", function(giphy2) {
-    // console.log(giphy1);
-    // console.log(giphy2);
+function giphyBattle(team1, team2) {
+  // console.log(team1);
+  // console.log(team2);
 
-    const trendingDate1 = giphy1.trending_datetime;
-    const trendingDate2 = giphy2.trending_datetime;
-    // "0000-00-00 00:00:00"
-    // "1970-01-01 00:00:00"
-    if (
-      (trendingDate1 === "0000-00-00 00:00:00" ||
-        trendingDate1 === "1970-01-01 00:00:00") &&
-      (trendingDate2 === "0000-00-00 00:00:00" ||
-        trendingDate2 === "1970-01-01 00:00:00")
-    ) {
-      if (giphy1.import_datetime > giphy2.import_datetime) {
-        console.log("gihpy1 won");
-      } else if (giphy1.import_datetime < giphy2.import_datetime) {
-        console.log("gihpy2 won");
-      } else {
-        console.log("they both lost");
-      }
+  const trendingDate1 = team1.trending_datetime;
+  const trendingDate2 = team2.trending_datetime;
+  // "0000-00-00 00:00:00"
+  // "1970-01-01 00:00:00"
+  if (
+    (trendingDate1 === "0000-00-00 00:00:00" ||
+      trendingDate1 === "1970-01-01 00:00:00") &&
+    (trendingDate2 === "0000-00-00 00:00:00" ||
+      trendingDate2 === "1970-01-01 00:00:00")
+  ) {
+    if (team1.import_datetime > team2.import_datetime) {
+      return "team1";
+    } else if (team1.import_datetime < team2.import_datetime) {
+      return "team2";
     } else {
-      if (giphy1.trending_datetime > giphy2.trending_datetime) {
-        console.log("gihpy1 won");
-      } else if (giphy1.trending_datetime < giphy2.trending_datetime) {
-        console.log("gihpy2 won");
-      } else {
-        console.log("they both lost");
+      return null;
+    }
+  } else {
+    if (team1.trending_datetime > team2.trending_datetime) {
+      return "team1";
+    } else if (team1.trending_datetime < team2.trending_datetime) {
+      return "team2";
+    } else {
+      return null;
+    }
+  }
+}
+
+function getNumberOfGifs(category, number) {
+  return new Promise(function(resolve, reject) {
+    const collection = [];
+    // make number of fetchRandomGif calls
+    for (let i = 0; i < number; i++) {
+      collection[i] = fetchRandomGif(category);
+    }
+
+    Promise.all(collection).then(function(values) {
+      console.log("promises", values);
+      for (let i = 0; i < number; i++) {
+        values[i] = values[i].data;
+      }
+      resolve(values);
+    });
+  });
+}
+
+function startBattle() {
+  console.log("1");
+  const teamSize = 6;
+  const team1Category = "obama";
+  const team2Category = "trump";
+
+  //make a team of six;
+  const team1Promise = getNumberOfGifs(team1Category, teamSize);
+  const team2Promise = getNumberOfGifs(team2Category, teamSize);
+
+  console.log("1", team1Promise);
+  console.log("2", team2Promise);
+
+  Promise.all([team1Promise, team2Promise]).then(function(values) {
+    console.log("teams", values);
+    const team1 = values[0];
+    const team2 = values[1];
+    //score
+    const score = {
+      team1: 0,
+      team2: 0
+    };
+    for (let i = 0; i < teamSize; i++) {
+      const result = giphyBattle(team1[i], team2[i]);
+      if (result === "team1") {
+        score.team1 = score.team1 + 1;
+      }
+      if (result === "team2") {
+        score.team2 = score.team2 + 1;
       }
     }
+    console.log("Score", score.team1, score.team2);
+    if (score.team1 === 0 || score.team2 === 0) {
+      console.log("BOOM SHUT OUT! KO! FINISH HIM!");
+    }
+    if (score.team1 > score.team2) {
+      return console.log("TEAM 1 is the Winner!");
+    } else if (score.team1 < score.team2) {
+      return console.log("TEAM 2 is the Winner!");
+    } else {
+      console.log("No Winner go again!");
+      return startBattle();
+    }
   });
-});
+}
+startBattle();
